2e706654bf8e70d0a5edf28b99dff9e021d00105
Yurii Pachevskyi <yurii.pachevskyi@globallogic.com>
2018-12-12 13:30:45 +0000

    [i2c_controller] improve code

--- i2c_controller.py
+++ i2c_controller.py
@@ -37,6 +37,7 @@ class I2CReadController(I2CController):
         self.callback = callback
         self.init_inputs(inputsMap)
         self.bus1 = smbus.SMBus(1)
+        self.bus2 = smbus.SMBus(2)
         self.i2c_read()
 
     def init_inputs(self, inputs):
@@ -52,22 +53,36 @@ class I2CReadController(I2CController):
     def is_input_state_changed(self, mask, value):
         return (value & mask) ^ mask
 
+    def try_to_notify(self, targetKey):
+        keyForNotify = None
+        for key in self.expanderState:
+            if key[1:3] == targetKey:
+                keyForNotify = key
+        if keyForNotify != None:
+            self.callback(key, self.expanderState[key])
+            self.expanderState.pop(key, None)
+
+    def i2c_device_read(self, i2cDevice):
+        if i2cDevice == 1:
+            bus = self.bus1
+            i2cDeviceDict = self.i2cDeviceDict1
+        if i2cDevice == 2:
+            bus = self.bus2
+            i2cDeviceDict = self.i2cDeviceDict2
+
+        for key in i2cDeviceDict:
+            modifiedPins = self.is_input_state_changed(i2cDeviceDict[key], bus.read_byte(key))
+
+            if not modifiedPins:
+                if self.expanderState:
+                    self.try_to_notify(str(key))
+            else:
+                exStKey = "1" + str(key) + str(modifiedPins)
+                self.expanderState[exStKey] = self.expanderState.get(exStKey, 0) + 1
+
     def i2c_read(self):
         while True:
-            for key in self.i2cDeviceDict1:
-                modifiedPins = self.is_input_state_changed(self.i2cDeviceDict1[key], self.bus1.read_byte(key))
-
-                if not modifiedPins:
-                    if self.expanderState:
-                        keyForNotify = None
-                        for k in self.expanderState:
-                            if k[1:3] == str(key):
-                                keyForNotify = k
-                        if keyForNotify != None:
-                                self.callback(k, self.expanderState[k])
-                                self.expanderState.pop(k, None)
-                else:
-                    exStKey = "1" + str(key) + str(modifiedPins)
-                    self.expanderState[exStKey] = self.expanderState.get(exStKey, 0) + 1
+            self.i2c_device_read(1)
+            self.i2c_device_read(2)
             time.sleep(settings.i2cReadTimeout)
 
