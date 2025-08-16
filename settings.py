264b2d1787e9202e194ad0e743e2d6a7601284e9
Yurii Pachevskyi <Yurii.Pachevskyi@partner.bmw.de>
2025-08-16 10:31:09 +0300

    Adopt homeControll to new version Home Assistant version

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 home = str(Path.home())
-confInputsFile = '/home/yuso/homeControll/scripts/configuration/inputs.json'
-confSwitchesFile = '/home/yuso/homeControll/switches.yaml'
-confSwitchesStateFile = '/home/yuso/homeControll/scripts/configuration/switches_state.yaml'
+confInputsFile = '/home/yurii/docker/homeControll/scripts/configuration/inputs.json'
+confSwitchesFile = '/home/yurii/docker/homeControll/switches.yaml'
+confSwitchesStateFile = '/home/yurii/docker/homeControll/scripts/configuration/switches_state.yaml'
 i2cMaxValue = 255
 i2cReadTimeout = 0.08
 mqttMainPath = "home/main/#"
