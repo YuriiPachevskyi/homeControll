83338ea1a52ad19e049008e578d3801a31a586a0
Yurii Pachevskyi <Yurii.Pachevskyi@partner.bmw.de>
2026-02-24 13:37:38 +0000

    Update dasboard add Deye5 Deye6
    Change-Id: I7a5480143a1d9442a04d2d8400c688cd766a86fe

--- configuration/custom_components/bms_ble/config_flow.py
+++ configuration/custom_components/bms_ble/config_flow.py
@@ -6,7 +6,6 @@ from typing import Any, Final
 from aiobmsble.utils import bms_identify
 import voluptuous as vol
 
-from custom_components.bms_ble.const import DOMAIN, LOGGER
 from homeassistant import config_entries
 from homeassistant.components.bluetooth import (
     BluetoothServiceInfoBleak,
@@ -21,30 +20,33 @@ from homeassistant.helpers.selector import (
     SelectSelectorConfig,
 )
 
+from .const import DOMAIN, LOGGER
+
+
+@dataclass
+class DiscoveredDevice:
+    """A discovered Bluetooth device."""
+
+    name: str
+    discovery_info: BluetoothServiceInfoBleak
+    type: str
+
+    def model(self) -> str:
+        """Return BMS type in capital letters, e.g. 'DUMMY BMS'."""
+        return self.type.rsplit(".", 1)[1].replace("_", " ").upper()
+
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for BT Battery Management System."""
 
-    VERSION = 2
+    VERSION = 3
     MINOR_VERSION = 0
-
-    @dataclass
-    class DiscoveredDevice:
-        """A discovered Bluetooth device."""
-
-        name: str
-        discovery_info: BluetoothServiceInfoBleak
-        type: str
-
-        def model(self) -> str:
-            """Return BMS type in capital letters, e.g. 'DUMMY BMS'."""
-            return self.type.rsplit(".", 1)[-1].replace("_", " ").upper()
 
     def __init__(self) -> None:
         """Initialize the config flow."""
 
-        self._disc_dev: ConfigFlow.DiscoveredDevice | None = None
-        self._disc_devs: dict[str, ConfigFlow.DiscoveredDevice] = {}
+        self._disc_dev: DiscoveredDevice | None = None
+        self._disc_devs: dict[str, DiscoveredDevice] = {}
 
     async def _async_device_supported(
         self, discovery_info: BluetoothServiceInfoBleak
@@ -70,18 +72,19 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle a flow initialized by Bluetooth discovery."""
         LOGGER.debug("Bluetooth device detected: %s", discovery_info)
 
-        await self.async_set_unique_id(discovery_info.address)
+        address: Final = discovery_info.address
+        await self.async_set_unique_id(address)
         self._abort_if_unique_id_configured()
 
         if not (bms_module := await self._async_device_supported(discovery_info)):
             return self.async_abort(reason="not_supported")
 
-        self._disc_dev = ConfigFlow.DiscoveredDevice(
+        self._disc_dev = DiscoveredDevice(
             discovery_info.name, discovery_info, bms_module
         )
         self.context["title_placeholders"] = {
             CONF_NAME: self._disc_dev.name,
-            CONF_ID: self._disc_dev.discovery_info.address[8:],  # remove OUI
+            CONF_ID: address[-9:],  # remove OUI
             CONF_MODEL: self._disc_dev.model(),
         }
         return await self.async_step_bluetooth_confirm()
@@ -110,10 +113,13 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the user step to pick discovered device."""
-        LOGGER.debug("user step")
+        LOGGER.debug(
+            "step user for %s",
+            user_input[CONF_ADDRESS] if user_input else "selection",
+        )
 
         if user_input is not None:
-            address: str = str(user_input[CONF_ADDRESS])
+            address = str(user_input[CONF_ADDRESS])
             await self.async_set_unique_id(address, raise_on_progress=False)
             self._abort_if_unique_id_configured()
             self._disc_dev = self._disc_devs[address]
@@ -123,24 +129,26 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 data={"type": self._disc_dev.type},
             )
 
-        current_addresses: Final[set[str | None]] = self._async_current_ids()
-        for discovery_info in async_discovered_service_info(self.hass, False):
+        current_addresses: Final = self._async_current_ids(include_ignore=False)
+        for discovery_info in list(
+            async_discovered_service_info(self.hass, connectable=True)
+        ):
             address = discovery_info.address
             if address in current_addresses or address in self._disc_devs:
                 continue
             if not (bms_module := await self._async_device_supported(discovery_info)):
                 continue
 
-            self._disc_devs[address] = ConfigFlow.DiscoveredDevice(
+            self._disc_devs[address] = DiscoveredDevice(
                 discovery_info.name, discovery_info, bms_module
             )
 
         if not self._disc_devs:
             return self.async_abort(reason="no_devices_found")
 
-        titles: list[SelectOptionDict] = []
+        devices: list[SelectOptionDict] = []
         for address, discovery in self._disc_devs.items():
-            titles.append(
+            devices.append(
                 SelectOptionDict(
                     value=address,
                     label=f"{discovery.name} ({address}) - {discovery.model()}",
@@ -152,7 +160,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema(
                 {
                     vol.Required(CONF_ADDRESS): SelectSelector(
-                        SelectSelectorConfig(options=titles)
+                        SelectSelectorConfig(options=devices),
                     )
                 }
             ),
--- configuration/custom_components/bms_ble/sensor.py
+++ configuration/custom_components/bms_ble/sensor.py
@@ -5,8 +5,12 @@ from typing import Final, cast
 
 from aiobmsble import BMSpackvalue, BMSSample
 
-from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
-from homeassistant.components.sensor.const import SensorDeviceClass, SensorStateClass
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntity,
+    SensorEntityDescription,
+    SensorStateClass,
+)
 from homeassistant.const import (
     ATTR_BATTERY_LEVEL,
     ATTR_TEMPERATURE,
@@ -28,6 +32,7 @@ from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from . import BTBmsConfigEntry
 from .const import (
+    ATTR_BATTERY_HEALTH,
     ATTR_CURRENT,
     ATTR_CYCLE_CAP,
     ATTR_CYCLES,
@@ -38,6 +43,7 @@ from .const import (
     ATTR_POWER,
     ATTR_RSSI,
     ATTR_RUNTIME,
+    ATTR_TEMP_SENSORS,
     DOMAIN,
     LOGGER,
 )
@@ -49,8 +55,9 @@ PARALLEL_UPDATES = 0
 class BmsEntityDescription(SensorEntityDescription, frozen_or_thawed=True):
     """Describes BMS sensor entity."""
 
-    value_fn: Callable[[BMSSample], float | int | None]
     attr_fn: Callable[[BMSSample], dict[str, list[int | float]]] | None = None
+    optional: bool = False
+    value_fn: Callable[[BMSSample], float | int | None]
 
 
 def _attr_pack(
@@ -66,163 +73,161 @@ def _attr_pack(
 
 SENSOR_TYPES: Final[list[BmsEntityDescription]] = [
     BmsEntityDescription(
+        attr_fn=lambda data: _attr_pack(data, "pack_voltages", [0.0]),
+        device_class=SensorDeviceClass.VOLTAGE,
         key=ATTR_VOLTAGE,
-        translation_key=ATTR_VOLTAGE,
         native_unit_of_measurement=UnitOfElectricPotential.VOLT,
         state_class=SensorStateClass.MEASUREMENT,
-        device_class=SensorDeviceClass.VOLTAGE,
         suggested_display_precision=1,
         value_fn=lambda data: data.get("voltage"),
-        attr_fn=lambda data: _attr_pack(data, "pack_voltages", [0.0]),
     ),
     BmsEntityDescription(
+        attr_fn=lambda data: _attr_pack(data, "pack_battery_levels", [0.0]),
+        device_class=SensorDeviceClass.BATTERY,
         key=ATTR_BATTERY_LEVEL,
-        translation_key=ATTR_BATTERY_LEVEL,
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
-        device_class=SensorDeviceClass.BATTERY,
         value_fn=lambda data: data.get("battery_level"),
-        attr_fn=lambda data: _attr_pack(data, "pack_battery_levels", [0.0]),
     ),
     BmsEntityDescription(
+        key=ATTR_BATTERY_HEALTH,
+        native_unit_of_measurement=PERCENTAGE,
+        state_class=SensorStateClass.MEASUREMENT,
+        translation_key=ATTR_BATTERY_HEALTH,
+        optional=True,
+        value_fn=lambda data: data.get("battery_health"),
+    ),
+    BmsEntityDescription(
+        attr_fn=lambda data: (
+            {ATTR_TEMP_SENSORS: data.get("temp_values", [])}
+            if "temp_values" in data
+            else {ATTR_TEMP_SENSORS: [data.get("temperature", 0.0)]}
+            if "temperature" in data
+            else {}
+        ),
+        device_class=SensorDeviceClass.TEMPERATURE,
         key=ATTR_TEMPERATURE,
-        translation_key=ATTR_TEMPERATURE,
         native_unit_of_measurement=UnitOfTemperature.CELSIUS,
         state_class=SensorStateClass.MEASUREMENT,
-        device_class=SensorDeviceClass.TEMPERATURE,
         suggested_display_precision=1,
         value_fn=lambda data: data.get("temperature"),
+    ),
+    BmsEntityDescription(
         attr_fn=lambda data: (
-            {"temperature_sensors": data.get("temp_values", [])}
-            if "temp_values" in data
-            else (
-                {"temperature_sensors": [data.get("temperature", 0.0)]}
-                if "temperature" in data
+            (
+                {"balance_current": [data.get("balance_current", 0.0)]}
+                if "balance_current" in data
                 else {}
             )
+            | _attr_pack(data, "pack_currents", [0.0])
         ),
-    ),
-    BmsEntityDescription(
+        device_class=SensorDeviceClass.CURRENT,
         key=ATTR_CURRENT,
-        translation_key=ATTR_CURRENT,
         native_unit_of_measurement=UnitOfElectricCurrent.AMPERE,
         state_class=SensorStateClass.MEASUREMENT,
-        device_class=SensorDeviceClass.CURRENT,
+        translation_key=ATTR_CURRENT,
         value_fn=lambda data: data.get("current"),
-        attr_fn=lambda data: (
-            {"balance_current": [data.get("balance_current", 0.0)]}
-            if "balance_current" in data
-            else {}
-        )
-        | _attr_pack(data, "pack_currents", [0.0]),
     ),
     BmsEntityDescription(
+        device_class=SensorDeviceClass.ENERGY_STORAGE,
         key=ATTR_CYCLE_CAP,
-        translation_key=ATTR_CYCLE_CAP,
         native_unit_of_measurement=UnitOfEnergy.WATT_HOUR,
         state_class=SensorStateClass.MEASUREMENT,
-        device_class=SensorDeviceClass.ENERGY_STORAGE,
         suggested_display_precision=1,
         value_fn=lambda data: data.get("cycle_capacity"),
     ),
     BmsEntityDescription(
-        key=ATTR_CYCLES,
-        translation_key=ATTR_CYCLES,
-        name="Cycles",
-        state_class=SensorStateClass.TOTAL_INCREASING,
-        value_fn=lambda data: data.get("cycles"),
         attr_fn=lambda data: _attr_pack(data, "pack_cycles", [0]),
+        key=ATTR_CYCLES,
+        state_class=SensorStateClass.TOTAL_INCREASING,
+        translation_key=ATTR_CYCLES,
+        value_fn=lambda data: data.get("cycles"),
     ),
     BmsEntityDescription(
+        device_class=SensorDeviceClass.POWER,
         key=ATTR_POWER,
-        translation_key=ATTR_POWER,
         native_unit_of_measurement=UnitOfPower.WATT,
         state_class=SensorStateClass.MEASUREMENT,
-        device_class=SensorDeviceClass.POWER,
         suggested_display_precision=1,
         value_fn=lambda data: data.get("power"),
     ),
     BmsEntityDescription(
+        device_class=SensorDeviceClass.DURATION,
         key=ATTR_RUNTIME,
-        translation_key=ATTR_RUNTIME,
-        name="Runtime",
         native_unit_of_measurement=UnitOfTime.SECONDS,
         suggested_unit_of_measurement=UnitOfTime.HOURS,
         state_class=SensorStateClass.MEASUREMENT,
-        device_class=SensorDeviceClass.DURATION,
+        translation_key=ATTR_RUNTIME,
         value_fn=lambda data: data.get("runtime"),
     ),
     BmsEntityDescription(
-        key=ATTR_DELTA_VOLTAGE,
-        translation_key=ATTR_DELTA_VOLTAGE,
-        name="Delta cell voltage",
-        native_unit_of_measurement=UnitOfElectricPotential.VOLT,
-        state_class=SensorStateClass.MEASUREMENT,
-        device_class=SensorDeviceClass.VOLTAGE,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        suggested_display_precision=3,
-        value_fn=lambda data: data.get("delta_voltage"),
         attr_fn=lambda data: (
             {"cell_voltages": data.get("cell_voltages", [])}
             if "cell_voltages" in data
             else {}
         ),
-    ),
-    BmsEntityDescription(
-        key=ATTR_MAX_VOLTAGE,
-        translation_key=ATTR_MAX_VOLTAGE,
-        name="Maximal cell voltage",
-        native_unit_of_measurement=UnitOfElectricPotential.VOLT,
-        state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.VOLTAGE,
         entity_category=EntityCategory.DIAGNOSTIC,
+        key=ATTR_DELTA_VOLTAGE,
+        native_unit_of_measurement=UnitOfElectricPotential.VOLT,
+        state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=3,
+        translation_key=ATTR_DELTA_VOLTAGE,
+        value_fn=lambda data: data.get("delta_voltage"),
+    ),
+    BmsEntityDescription(
+        attr_fn=lambda data: (
+            {"cell_number": [cells.index(max(cells))]}
+            if (cells := data.get("cell_voltages", []))
+            else {}
+        ),
+        device_class=SensorDeviceClass.VOLTAGE,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        entity_registry_enabled_default=False,
+        key=ATTR_MAX_VOLTAGE,
+        native_unit_of_measurement=UnitOfElectricPotential.VOLT,
+        state_class=SensorStateClass.MEASUREMENT,
+        suggested_display_precision=3,
+        translation_key=ATTR_MAX_VOLTAGE,
         value_fn=lambda data: (
             max(cells) if (cells := data.get("cell_voltages", [])) else None
         ),
+    ),
+    BmsEntityDescription(
         attr_fn=lambda data: (
-            {"cell_number": cells.index(max(cells))}
+            {"cell_number": [cells.index(min(cells))]}
             if (cells := data.get("cell_voltages", []))
             else {}
         ),
-    ),
-    BmsEntityDescription(
-        key=ATTR_MIN_VOLTAGE,
-        translation_key=ATTR_MIN_VOLTAGE,
-        name="Minimal cell voltage",
-        native_unit_of_measurement=UnitOfElectricPotential.VOLT,
-        state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.VOLTAGE,
         entity_category=EntityCategory.DIAGNOSTIC,
+        entity_registry_enabled_default=False,
+        key=ATTR_MIN_VOLTAGE,
+        native_unit_of_measurement=UnitOfElectricPotential.VOLT,
+        state_class=SensorStateClass.MEASUREMENT,
         suggested_display_precision=3,
+        translation_key=ATTR_MIN_VOLTAGE,
         value_fn=lambda data: (
             min(cells) if (cells := data.get("cell_voltages", [])) else None
         ),
-        attr_fn=lambda data: (
-            {"cell_number": cells.index(min(cells))}
-            if (cells := data.get("cell_voltages", []))
-            else {}
-        ),
     ),
     BmsEntityDescription(
+        device_class=SensorDeviceClass.SIGNAL_STRENGTH,
+        entity_category=EntityCategory.DIAGNOSTIC,
+        entity_registry_enabled_default=False,
         key=ATTR_RSSI,
-        translation_key=ATTR_RSSI,
         native_unit_of_measurement=SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
         state_class=SensorStateClass.MEASUREMENT,
-        device_class=SensorDeviceClass.SIGNAL_STRENGTH,
-        entity_registry_enabled_default=False,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        value_fn=lambda data: None,  # RSSI is handled in a separate class
+        value_fn=lambda data: None,
     ),
     BmsEntityDescription(
+        entity_category=EntityCategory.DIAGNOSTIC,
+        entity_registry_enabled_default=False,
         key=ATTR_LQ,
-        translation_key=ATTR_LQ,
-        name="Link quality",
         native_unit_of_measurement=PERCENTAGE,
         state_class=SensorStateClass.MEASUREMENT,
-        entity_registry_enabled_default=False,
-        entity_category=EntityCategory.DIAGNOSTIC,
-        value_fn=lambda data: None,  # LQ is handled in a separate class
+        translation_key=ATTR_LQ,
+        value_fn=lambda data: None,
     ),
 ]
 
@@ -234,16 +239,22 @@ async def async_setup_entry(
 ) -> None:
     """Add sensors for passed config_entry in Home Assistant."""
 
-    bms: Final[BTBmsCoordinator] = config_entry.runtime_data
-    mac: Final[str] = format_mac(config_entry.unique_id)
+    bms: Final = config_entry.runtime_data
+    mac: Final = format_mac(config_entry.unique_id)
+
+    entities: list[SensorEntity] = []
     for descr in SENSOR_TYPES:
         if descr.key == ATTR_RSSI:
-            async_add_entities([RSSISensor(bms, descr, mac)])
+            entities.append(RSSISensor(bms, descr, mac))
             continue
         if descr.key == ATTR_LQ:
-            async_add_entities([LQSensor(bms, descr, mac)])
+            entities.append(LQSensor(bms, descr, mac))
             continue
-        async_add_entities([BMSSensor(bms, descr, mac)])
+        if descr.optional and descr.key not in bms.data:
+            continue
+        entities.append(BMSSensor(bms, descr, mac))
+
+    async_add_entities(entities)
 
 
 class BMSSensor(CoordinatorEntity[BTBmsCoordinator], SensorEntity):
@@ -255,7 +266,7 @@ class BMSSensor(CoordinatorEntity[BTBmsCoordinator], SensorEntity):
     def __init__(
         self, bms: BTBmsCoordinator, descr: BmsEntityDescription, unique_id: str
     ) -> None:
-        """Intitialize the BMS sensor."""
+        """Initialize the BMS sensor."""
         self._attr_unique_id = f"{DOMAIN}-{unique_id}-{descr.key}"
         self._attr_device_info = bms.device_info
         self.entity_description = descr
@@ -264,7 +275,7 @@ class BMSSensor(CoordinatorEntity[BTBmsCoordinator], SensorEntity):
     @property
     def extra_state_attributes(self) -> dict[str, list[int | float]] | None:
         """Return entity specific state attributes, e.g. cell voltages."""
-        if self.entity_description.attr_fn:
+        if self.coordinator.data and self.entity_description.attr_fn:
             return self.entity_description.attr_fn(self.coordinator.data)
 
         return None
@@ -272,25 +283,28 @@ class BMSSensor(CoordinatorEntity[BTBmsCoordinator], SensorEntity):
     @property
     def native_value(self) -> int | float | None:
         """Return the sensor value."""
-        return self.entity_description.value_fn(self.coordinator.data)
+        return (
+            self.entity_description.value_fn(self.coordinator.data)
+            if self.coordinator.data
+            else None
+        )
 
 
 class RSSISensor(SensorEntity):
     """The Bluetooth RSSI sensor."""
 
-    LIMIT: Final[int] = 127  # limit to +/- this range
+    LIMIT: Final = 127  # limit to +/- this range
     _attr_has_entity_name = True
-    _attr_native_value = -LIMIT
 
     def __init__(
         self, bms: BTBmsCoordinator, descr: SensorEntityDescription, unique_id: str
     ) -> None:
-        """Intitialize the BMS sensor."""
+        """Initialize the BMS sensor."""
 
         self._attr_unique_id = f"{DOMAIN}-{unique_id}-{descr.key}"
         self._attr_device_info = bms.device_info
         self.entity_description = descr
-        self._bms: Final[BTBmsCoordinator] = bms
+        self._bms: Final = bms
 
     async def async_update(self) -> None:
         """Update RSSI sensor value."""
@@ -301,7 +315,6 @@ class RSSISensor(SensorEntity):
         self._attr_available = self._bms.rssi is not None
 
         LOGGER.debug("%s: RSSI value: %i dBm", self._bms.name, self._attr_native_value)
-        self.async_write_ha_state()
 
 
 class LQSensor(SensorEntity):
@@ -314,12 +327,12 @@ class LQSensor(SensorEntity):
     def __init__(
         self, bms: BTBmsCoordinator, descr: SensorEntityDescription, unique_id: str
     ) -> None:
-        """Intitialize the BMS link quality sensor."""
+        """Initialize the BMS link quality sensor."""
 
         self._attr_unique_id = f"{DOMAIN}-{unique_id}-{descr.key}"
         self._attr_device_info = bms.device_info
         self.entity_description = descr
-        self._bms: Final[BTBmsCoordinator] = bms
+        self._bms: Final = bms
 
     async def async_update(self) -> None:
         """Update BMS link quality sensor value."""
@@ -327,4 +340,3 @@ class LQSensor(SensorEntity):
         self._attr_native_value = self._bms.link_quality
 
         LOGGER.debug("%s: Link quality: %i %%", self._bms.name, self._attr_native_value)
-        self.async_write_ha_state()
