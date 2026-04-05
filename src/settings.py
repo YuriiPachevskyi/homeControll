44e34c5120f3d74e465d2982812b163fdbe09353
Yurii Pachevskyi <Yurii.Pachevskyi@partner.bmw.de>
2026-04-05 08:49:32 +0100

    Return back to homeControll service
    Change-Id: I75c9626a431d605067941b2b1bf52e11fbc91080

--- src/settings.py
+++ src/settings.py
@@ -2,8 +2,9 @@ from pathlib import Path
 import os
 
 home = str(Path.home())
-confSwitchesFile = os.getenv('CONFIG_PATH', '/config/switches.yaml')
-confInputsFile = os.getenv('INPUTS_PATH', '/config/inputs.json')
+confSwitchesFile = os.getenv('CONFIG_PATH', '/home/yurii/docker/homeControll/configuration/switches.yaml')
+confInputsFile = os.getenv('INPUTS_PATH', '/home/yurii/docker/homeControll/configuration/inputs.json')
+
 
 # Timings in seconds
 DEBOUNCE_TIME = 0.05    # Ignore pulses shorter than 50ms (noise)
